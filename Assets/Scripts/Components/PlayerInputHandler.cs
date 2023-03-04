a0f78c2be33f44168dc5a99562632b8c5233b3e3
zoniventris <f.zoniventris@gmail.com>
2023-03-04 12:37:51 -0800

    Allow movement with mouse.

--- Assets/Scripts/Components/PlayerInputHandler.cs
+++ Assets/Scripts/Components/PlayerInputHandler.cs
@@ -10,18 +10,50 @@ public class PlayerInputHandler : MonoBehaviour
     [Tooltip("Maximum move speed in units/second")]
     private float maxMoveSpeed;
 
-    private PlayerInput playerInput;
+    [SerializeField]
+    private float destinationReachedTolerance = 0.5f;
+
     private Rigidbody2D rb;
+    private Vector2 mousePos;
+    private Camera mainCamera;
+
+    public Vector3 CurrentDestination { get; private set; }
 
     private void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
     }
 
     private void OnMove(InputValue value)
     {
         Vector2 inVector = value.Get<Vector2>();
         rb.velocity = inVector * maxMoveSpeed;
+    }
+
+    private void OnPoint(InputValue value)
+    {
+        mousePos = value.Get<Vector2>();
+    }
+
+    private void OnMoveTo()
+    {
+        CurrentDestination = mainCamera.ScreenToWorldPoint(mousePos);
+        CurrentDestination = new Vector3(CurrentDestination.x, CurrentDestination.y, 0);
+        Vector3 direction = (CurrentDestination - transform.position).normalized;
+        rb.velocity = direction * maxMoveSpeed;
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.magnitude > 0)
+        {
+            float distance = Vector3.Distance(transform.position, CurrentDestination);
+            Debug.Log(distance);
+            if (distance < destinationReachedTolerance)
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
     }
 }
